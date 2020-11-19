36e314c8a48d3b55622bf122a1708503eaac127c
kwaneung <kwaneung.kim@outlook.com>
2020-11-19 11:40:39 +0900

    58.236.43.217:5000/dev/test-rest:0.7
    포트번호 오타 수정

--- server.py
+++ server.py
@@ -39,5 +39,5 @@ class RegistUser(Resource):
 api.add_resource(RegistUser, '/user')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8008, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
     # app.run(debug=True)
