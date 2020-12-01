cc4ccdf4e94af5d009a95441c32d9bc1f9360f20
kwaneung <kwaneung.kim@outlook.com>
2020-12-01 20:30:39 +0900

    58.236.43.217:5000/dev/test-rest:0.8
    데이터베이스 스키마 구축 및 로그인 테스트

--- RESTAPI-server/server.py
+++ RESTAPI-server/server.py
@@ -6,8 +6,8 @@ import pymysql
 app = Flask(__name__)
 api = Api(app)
 
-connect = pymysql.connect(host='192.168.0.40', user='root', password='0000',
-                          db='kwaneung', charset='utf8')
+connect = pymysql.connect(host='192.168.0.40', user='etfuser', password='1q2w3e4r',
+                          db='ETFIPS', charset='utf8')
 
 cur = connect.cursor()
 
@@ -21,12 +21,15 @@ class RegistUser(Resource):
 
         sql = "select * from user"
         cur.execute(sql)
-        rows = cur.fetchall()
+        rows = cur.fetchall()  # id, name, passwd, money, max_prft_pct, min_loss_pct, prft_from_prev_mon, loss_from_prev_mon
 
         ID = args['ID']
         passwd = args['passwd']
         print('Request ID : ' + ID)
         print('Request passwd : ' + passwd)
+
+        rows = [i[1:3] for i in rows]
+        print(rows)
 
         if (ID, passwd) in rows:
             print('Result : True')
@@ -39,5 +42,5 @@ class RegistUser(Resource):
 api.add_resource(RegistUser, '/user')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
-    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)  # push
+    # app.run(debug=True)  # local test
