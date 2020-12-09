e7c7245c4764cc0404082576e34e6383b9f0d861
kwaneung <kwaneung.kim@outlook.com>
2020-12-09 21:30:38 +0900

    58.236.43.217:5000/dev/test-rest:1.1
    DAO의 SQL 예외처리
    SQL 실행 로직 함수 작성
    commit 함수 삭제 -> connect.commit() 사용

--- RESTAPI-server/user_DAO.py
+++ RESTAPI-server/user_DAO.py
@@ -12,11 +12,17 @@ def dbConnect(host, user, password, db):
         raise
 
 
-def commit(connect):
-    cur = connect.cursor()
-    sql = """commit"""
-    cur.execute(sql)
-    return
+def sqlExcute(connect, sql, var):
+    try:
+        cur = connect.cursor()
+        cur.execute(sql, var)
+        connect.commit()
+        return True
+    except Exception as e:
+        logging.error(e)
+        logging.exception(e)
+        return False
+        raise
 
 
 def getUser():
@@ -37,11 +43,12 @@ def insertUser(id, password):
         print("Connection fail")
         return False
     else:
-        cur = connect.cursor()
+        # cur = connect.cursor()
         sql = """insert into user values(%s,%s)"""
-        cur.execute(sql, (id, password))
-        commit(connect)
-        return True
+        # cur.execute(sql, (id, password))
+        # connect.commit()
+        # return True
+        return sqlExcute(connect, sql, (id, password))
 
 
 def updateUser(id, password):
@@ -50,11 +57,12 @@ def updateUser(id, password):
         print("Connection fail")
         return False
     else:
-        cur = connect.cursor()
+        # cur = connect.cursor()
         sql = """update user set password=%s where id=%s"""
-        cur.execute(sql, (password, id))
-        commit(connect)
-        return True
+        # cur.execute(sql, (password, id))
+        # connect.commit()
+        # return True
+        return sqlExcute(connect, sql, (password, id))
 
 
 def deleteUser(id):
@@ -63,24 +71,24 @@ def deleteUser(id):
         print("Connection fail")
         return False
     else:
-        cur = connect.cursor()
+        # cur = connect.cursor()
         sql = """delete from user where id=%s"""
-        cur.execute(sql, id)
-        commit(connect)
-        return True
-
+        # cur.execute(sql, id)
+        # connect.commit()
+        # return True
+        return sqlExcute(connect, sql, id)
 
 if __name__ == '__main__':
     print("==========getUser==========")
     print(getUser())
     print("==========insertUser==========")
-    insertUser('DAOtest', '1q2w3e4r')
+    print(insertUser('DAOtest', '1q2w3e4r'))
     print(getUser())
     print("==========updateUser==========")
-    updateUser("DAOtest", "zaq1@WSX")
+    print(updateUser("DAOtest", "zaq1@WSX"))
     print(getUser())
     print("==========deleteUser==========")
-    deleteUser('DAOtest')
+    print(deleteUser('DAOtest'))
     print(getUser())
     print("==========getUser==========")
     print(getUser())
